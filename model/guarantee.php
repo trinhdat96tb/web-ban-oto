0f8eef718195034cd386c112c21cea7d1d31b4cb
congdat.trinh <congdat.trinh@tanchonggroup.com>
2018-09-26 17:53:25 +0700

    update new version

---
+++ model/guarantee.php
@@ -0,0 +1,71 @@
+<html>
+    <head>
+        <meta http-equiv=”Content-Type” content=”text/html; charset=UTF-8″ />
+        <link rel="stylesheet" type="text/css" href="../css/style_sunny.css" />
+        <link rel="stylesheet" type="text/css" href="../css/service.css" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <style>
+            .style{
+                color:black;
+            }
+        </style>
+    </head>
+    <body>
+        <?php include("header.php") ?>
+        
+        <div class="wapper">
+            <div class="container" style="padding-top:140px">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2 style="color:red">BẢO HÀNH</h2><br>
+                        <h4 class="style">Tổng quan về bảo hành xe mới</h5>
+                        <p>Nissan Việt Nam, từ nay xin được gọi tắt là “Nissan”, 
+                            đảm bảo rằng trong khoảng thời gian quy định hoặc số km nhất định, 
+                            như được trình bày trong phần “Thời gian bảo hành xe mới”, Nissan sẽ sửa chữa 
+                            hoặc thay thế miễn phí bất kỳ phụ tùng nào trên xe được xác nhận là bị hư hỏng 
+                            do vật liệu hoặc do trình độ tay nghề, trừ những hạng mục được liệt kê trong mục 
+                            “Những gì không được bảo hành” và thỏa mãn thời gian và điều kiện trong chính sách bảo hành xe mới của Nissan.</p>
+                        <p class="style">Bảo hành xe NISSAN mới</p>
+                        <p>Để giúp khách hàng an tâm cũng như có những trải nghiệm thú vị khi lái xe, Nissan đảm bảo hoạt động của xe trong những điều kiện nhất định. Chúng tôi xin trân trọng giới thiệu ngắn gọn về chính sách bảo hành xe mới của Nissan với mong muốn đem lại lợi ích cho khách hàng.</p>
+                        <p>**Thời gian bảo hành cho xe mới là 36 tháng hoặc 100.000 km (60.000 dặm), tùy theo điều kiện nào đến trước. Bảo hành xe mới bao gồm tất cả các chi tiết và các bộ phận của xe NISSAN mới được cung cấp bởi NISSAN trừ lốp và những hạng mục được liệt kê trong phần “Những gì không được bảo hành” ở trang này</p>
+                        <p>Đối với ắc quy, điều chỉnh và nạp lại ga điều hòa thì thời gian bảo hành sẽ khác so với bảo hành xe mới. Nội dung cụ thể sẽ được đề cập đến trong phần “Những điều bạn nên biết về chính sách bảo hành xe mới của NISSAN” được chỉ ra dưới đây :</p>
+                        <p>Những điều bạn nên biết về chính sách bảo hành xe mới của NISSAN</p>
+                        <p>1. <strong class="style">Các điều chỉnh:</strong> Các điều chỉnh không thường xuyên gắn liền với việc thay thế phụ tùng được bảo hành trong 12 tháng đầu tiên không tính đến số km đã đi.</p>
+                        <p>2. <strong class="style">Ga điều hòa:</strong> Các bộ phận nguyên bản của hệ thống điều hòa lắp trên xe mới được bảo hành như trong bảo hành xe mới, 36 tháng hoặc 100.000 km (60.000 dặm). . Tuy nhiên, việc nạp lại ga điều hòa không liên quan đến việc sửa chữa hay thay thế các bộ phận bảo hành của điều hòa sẽ được bảo hành trong 12 tháng đầu tiên, không tính đến số km đã đi được.</p>
+                        <p>3. <strong class="style">Ắc quy nguyên bản:</strong> Thời gian bảo hành là 12 tháng hoặc 20.000km (12.000 dặm) kể từ ngày bán xe, tùy theo điều kiện nào đến trước. Nếu xe Nissan được sử dụng để kinh doanh hoặc thiết kế nguyên mẫu của xe bị thay đổi thì ắc quy sẽ chỉ được bảo hành trong vòng 6 tháng kể từ ngày bán hoặc quãng đường xe chạy chưa vượt quá 10.000km, tùy theo điều kiện nào đến trước</p>
+                        <p>4. <strong class="style">Dịch vụ kiểm tra trước khi giao xe (PDI):</strong> Để giúp bạn hoàn toàn hài lòng với chiếc xe mới, xe đã được kiểm tra toàn bộ tuân theo quy trình kiểm tra đối với xe mới của NISSAN.</p>
+                        <p>5. <strong class="style">Dịch vụ bảo dưỡng định kỳ:</strong> Bảo dưỡng và chăm sóc đúng đắn chiếc xe là điều thiết yếu cho việc lái xe an toàn và giảm chi phí vận hành xe. Việc bảo dưỡng định kỳ là yêu cầu tối thiểu và nên được thực hiện một cách chủ động ngay từ ban đầu. Có thể yêu cầu những dịch vụ bảo dưỡng bổ sung do điều kiện về thời tiết và áp suất khí quyển, thay đổi đường xá, cách sử dụng và thói quen lái xe của cá nhân góp phần rất lớn đến nhu cầu phải bảo dưỡng xe. Bất kỳ những hư hỏng hoặc lỗi gây ra do thiếu sự bảo dưỡng hoặc bảo dưỡng không chính xác sẽ không được bảo hành. </p>
+                        <p>6. <strong class="style">Đại lý bảo dưỡng sửa chữa:</strong> Bạn nên để dịch vụ bảo dưỡng miễn phí, bảo dưỡng định kỳ và dịch vụ bảo hành được thực hiện bởi Đại lý bán hàng xe NISSAN nơi bạn mua xe, do Đại lý đó có được lợi ích trực tiếp nếu bạn luôn cảm thấy hài lòng.</p>
+                        <p>7. <strong class="style">Phạm vi áp dụng:</strong> Chính sách bảo hành này được áp dụng cho xe NISSAN đã được đăng ký và hoạt động bình thường trong phạm vi quốc gia nơi xe được bán lần đầu tiên. Chính sách bảo hành này được áp dụng cho khách hàng đầu tiên sở hữu xe cũng như khách hàng mua lại xe nằm trong thời hạn bảo hành .</p>
+                        <p>8. <strong class="style">Dịch vụ bảo hành tạm thời:</strong> Nếu bạn sử dụng xe ở nước ngoài và và hư hỏng xảy ra với chiếc xe NISSAN, hãy liên hệ với Đại lý NISSAN của nước đó. Dịch vụ bảo hành sẽ cung cấp cho bạn dưới dạng tạm thời được quy định trong cuốn sách này. Tuy nhiên, hãy ghi nhớ rằng bất kỳ những hư hỏng liên quan đến việc thiếu tuân theo luật lệ hoặc yêu cầu về môi trường của nước đó sẽ không được bảo hành.</p>
+                        <p>9. <strong class="style">Lái xe ở nước ngoài:</strong> Chiếc xe NISSAN được chế tạo nhằm thỏa mãn những quy định và những yêu cầu về môi trường của quốc gia mà chiếc xe NISSAN được bán lần đầu tiên. Trong trường hợp bạn chuyển sang sinh sống ở nước khác và mang xe đi theo, chiếc xe có thể không thỏa mãn về quy định và yêu cầu về môi trường của nước đó. Sẽ rất khó để thay đổi hoặc sửa chữa chiếc xe để phù hợp với những quy định của nước mà bạn chuyển đến. Hơn nữa, thậm chí nếu bạn mang xe đến đại lý NISSAN ở nước ngoài để sửa chữa, phụ tùng cần thiết cho việc sửa chữa có thể không có tại đại lý đó, vì đại lý ở nước này có thể có các model hoặc các thông số kỹ thuật khác nhau. Vì vậy rất khó thực hiện sửa chữa ở nước đó. Hãy ghi nhớ rằng xe NISSAN được đưa sang nước ngoài sẽ không được bảo hành.</p>
+                        <br>
+                        <p><strong class="style">Chu kỳ bảo hành xe mới </strong></p>
+                        <p>Thời gian bảo hành xe mới được xác định theo khoảng thời gian quy định hoặc số km nhất định như chỉ ra dưới đây, và được tính bắt đầu từ ngày đăng kí xe đầu tiên hoặc bắt đầu đưa vào sử dụng, tùy theo điều kiện nào đến trước.</p>
+                        <p>Thời gian bảo hành: 3 năm</p>
+                        <p>Số km đã đi: 100,000 km</p>
+                        <br>
+                        <p><strong class="style">Nissan sẽ làm gì</strong></p>
+                        <p>Để giúp bạn hoàn toàn hài lòng với chiếc xe mới, trước khi giao xe tới tay bạn, tất cả các xe đã được kiểm tra toàn bộ tuân theo quy trình kiểm tra đối với xe mới của NISSAN.</p>
+                        <p>– Những công việc sửa chữa bảo hành sẽ được thực hiện miễn phí cả phụ tùng và/hoặc công lao động (trừ Ắc quy, trong trường hợp này bạn phải trả một lượng chi phí nhất định như được ghi chú trong phần “Những điều bạn cần biết về chính sách bảo hành xe mới của NISSAN” được chỉ ra trong mục “Thế nào là chính sách bảo hành xe mới của Nissan” ở trang này”) . Bất kỳ chi tiết nào cần thiết phải thay thế sẽ được thay mới hoặc chi tiết được phép phục hồi lại bởi Đại lý ủy quyền của NISSAN. Các đại lý phải dành một thời gian hợp lý để hoàn thành việc sửa chữa sau khi khách hàng đã giao xe cho Đại lý ủy quyền của NISSAN.</p>
+                        <p>– Dịch vụ kiểm tra miễn phí tại 1000 km (600 dặm) đầu tiên sẽ được thực hiện miễn phí trừ các chi phí thay dầu lọc dầu, dung dịch và chất bôi trơn.</p>
+                        <br>
+                        <p><strong class="style">Điều kiện bảo hành xe mới</strong></p>
+                        <p>– Để bảo dưỡng và chăm sóc xe một cách thích hợp, các hạng mục được chỉ ra trong cuốn “Hướng dẫn sử dụng” và PHẦN BẢO DƯỠNG của “Sổ bảo hành và theo dõi bảo dưỡng” là yêu cầu bảo dưỡng tối thiểu mà bạn nên thực hiện.</p>
+                        <p>– Hãy mang xe đến Đại lý được ủy quyền của NISSAN trong giờ làm việc hành chính thông thường để nhận được dịch vụ bảo hành.</p>
+                        <p>– Hãy kiểm tra những lỗi về nội thất, lỗi sơn và những lỗi bên ngoài khác tại thời điểm nhận xe mới và báo ngay những vấn đề đó cho người bán hàng của Đại lý.</p>
+                        <p>– Hãy giữ lại hồ sơ bảo dưỡng trong trường hợp có những câu hỏi phát sinh liên quan đến việc bảo dưỡng xe.</p>
+                        
+                        <br><br>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+        <br><br>
+        <?php include("footer.php") ?>
+    </body>
+</html>
--- model/header.php
+++ model/header.php
@@ -1,6 +1,27 @@
 <html>
     <head>
         <meta http-equiv=”Content-Type” content=”text/html; charset=UTF-8″ />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script type="text/javascript">
+            function RedirectHome() {
+               window.location="index.php";
+            }
+            function RedirectProduct() {
+               window.location="product.php";
+            }
+            function RedirectPriceList() {
+               window.location="pricelist.php";
+            }
+            function RedirectDealer() {
+               window.location="dealer.php";
+            }
+            function RedirectContact() {
+               window.location="contact.php";
+            }
+      </script>
     </head>
     
     <body>    
@@ -21,17 +42,66 @@
                             <input type="submit" class="search-submit" value="">
                         </form>
                     </div>
-                    <div style="padding-top:26px; margin-left:95px">
+                    <div style="padding-top:26px; margin-left:70px">
                         <nav class="navbar navbar-default" style="margin: 0 auto; margin-left:60px; border:0; background:white">
-                            <ul class="nav navbar-nav">
-                                <li><a href="index.php">HOME</a></li>
-                                <li><a href="product.php">PRODUCT</a></li>
-                                <li><a href="pricelist.php">PRICELIST</a></li>
-                                <li><a href="dealer.php">DEALER</a></li>
-                                <li><a href="services.php">SERVICES</a></li>
-                                <li><a href="nissan.php">NISSANVIETNAM</a></li>
-                                <li><a href="innovation.php">INNOVATION</a></li>
-                                <li><a href="contact.php">CONTACT</a></li>
+                            <ul class="nav navbar-nav" style="padding-top:12px">
+                                <li>
+                                    <div class="dropdown col-sm-1">
+                                        <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown" style="border:none" onClick="RedirectHome();">HOME
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="dropdown col-sm-1">
+                                        <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown" style="border:none" onClick="RedirectProduct();">PRODUCT
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="dropdown col-sm-1">
+                                        <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown" style="border:none" onClick="RedirectPriceList();">PRICELIST
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="dropdown col-sm-1">
+                                        <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown" style="border:none" onClick="RedirectDealer();">DEALER
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="dropdown col-sm-1">
+                                        <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown" style="border:none">SERVICE
+                                        <span class="caret"></span></button>
+                                        <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+                                        <li role="presentation"><a role="menuitem" tabindex="-1" href="services.php">Repair Service</a></li>
+                                        <li role="presentation"><a role="menuitem" tabindex="-1" href="accessary.php">Accessary</a></li>
+                                        <li role="presentation"><a role="menuitem" tabindex="-1" href="guarantee.php">Guarantee</a></li>
+                                        </ul>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="dropdown col-sm-1">
+                                        <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown" style="border:none">NISSAN VIETNAM
+                                        <span class="caret"></span></button>
+                                        <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+                                        <li role="presentation"><a role="menuitem" tabindex="-1" href="nissan.php">Nissan Vietnam</a></li>
+                                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">CEO Message</a></li>
+                                        </ul>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="dropdown col-sm-1">
+                                        <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown" style="border:none">INNOVATION
+                                        <span class="caret"></span></button>
+                                        <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+                                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Safety</a></li>
+                                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Entertainment</a></li>
+                                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Quality</a></li>
+                                        </ul>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="dropdown col-sm-1">
+                                        <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown" style="border:none" onClick="RedirectContact();">CONTACT
+                                    </div>
+                                </li>
                             </ul>
                         </nav>
                     </div>
